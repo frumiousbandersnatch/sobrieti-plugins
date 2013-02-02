2617f91aa98ac912a537f338a70252ce44600d2f
Frumious Bandersnatch <frumiousirc@gmail.com>
2013-02-02 18:21:04 -0500

    Move api to own module, let in-chan adds go more smoothly.

--- plugins/Qdb/plugin.py
+++ plugins/Qdb/plugin.py
@@ -34,60 +34,16 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
-from urllib import urlencode
-from urllib2 import urlopen, HTTPError
-import json
+import qdbapi
+reload (qdbapi)
 
-rashurl = 'http://stop.zzl.org/qdb'
-#rashurl = 'http://192.168.1.129/rqdb'
-apiurl = rashurl + '/api.php'
-
-def apicall(cmd, **params):
-    args = dict(cmd=cmd)
-    if params:
-        args.update(params)
-
-    url = apiurl + '?' + urlencode(args)
-    print 'APICALL',url
-
-    try: 
-        fp = urlopen(url)
-    except HTTPError, msg:
-        print msg
-        raise
-    contents = fp.read()
-    try:
-        data = json.loads(contents)
-    except ValueError,msg:
-        print msg
-        print contents
-        return None
-    return data
-
-def format_quote_list(data):
-    ids = ', '.join(['[%s]'%d['id'] for d in data])
-    return 'quotes: %s' % ids
-
-def format_quote(data, maxlen = 1000):
-    q = data['quote']
-    q = q.replace('&lt;','<').replace('&gt;','>').replace('\r\n',' | ')
-    q = q.replace('&quot;','"')
-    q = ' '.join(q.split())
-    n = data['id']
-    s = data['rating']
-    u = '%s/?%s' % (rashurl, n)
-    maybe = '#%s(%s): [%s] %s' % (n,s,u,q)
-    if len(maybe) < maxlen:
-        return maybe
-    return '#%s(%s): [ %s ] (long quote, see web page)' % (n,s,u)
-        
 class Qdb(callbacks.Plugin):
     """Add the help for "@plugin help Qdb" here
     This should describe *how* to use this plugin."""
     threaded = True
 
     def _apicall(self, irc, cmd, **kwds):
-        data = apicall(cmd, **kwds)
+        data = qdbapi.apicall(cmd, **kwds)
         if not data:
             irc.reply('(none)')
             return
@@ -96,11 +52,19 @@ class Qdb(callbacks.Plugin):
             return
 
         if isinstance(data,list):
-            q = format_quote_list(data)
+            q = qdbapi.format_quote_list(data)
         else:
-            q = format_quote(data)
+            q = qdbapi.format_quote(data)
         irc.reply(q, prefixNick=False)
         return
+
+    def qdb(self, irc, msg, args, number):
+        if not number:
+            self._apicall(irc,'random')
+            return
+        self._apicall(irc, 'get', qid=number)
+        return
+    qdb = wrap(qdb, [optional('int',0)])
 
     def random(self, irc, msg, args):
         self._apicall(irc, 'random')
@@ -129,13 +93,14 @@ class Qdb(callbacks.Plugin):
     def add(self, irc, msg, args, quote):
         print 'ADD',quote
         quote = ' '.join(quote)
-        data = apicall('add', quote=quote)
+        quote = qdbapi.wash_quote(quote)
+        data = qdbapi.apicall('add', quote=quote)
         irc.reply('Added quote #%s' % data['id'], prefixNick=False)
         return
     add = wrap(add, [many('something')])
-
 
 Class = Qdb
 
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
+
---
+++ plugins/Qdb/qdbapi.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+'''
+Upload/downoad QDB to pickle
+'''
+
+import pickle
+import json
+import re
+import cgi
+from urllib import urlencode
+from urllib2 import urlopen, HTTPError
+
+rashurl = 'http://stop.zzl.org/qdb'
+#rashurl = 'http://192.168.1.129/rqdb'
+apiurl = rashurl + '/api.php'
+
+def apicall(cmd, **params):
+    args = dict(cmd=cmd)
+    if params:
+        args.update(params)
+
+    url = apiurl + '?' + urlencode(args)
+    print 'APICALL',url
+
+    try: 
+        fp = urlopen(url)
+    except HTTPError, msg:
+        print msg
+        raise
+    contents = fp.read()
+    try:
+        data = json.loads(contents)
+    except ValueError,msg:
+        print msg
+        print contents
+        return None
+    return data
+
+
+def format_quote_list(data):
+    ids = ', '.join(['[%s]'%d['id'] for d in data])
+    return 'quotes: %s' % ids
+
+def format_quote(data, maxlen = 1000):
+    '''
+    Format the data returned from an API get into a single line.
+
+    Punt if it is longer than maxlen.
+    '''
+    q = data['quote']
+    q = q.replace('&lt;','<').replace('&gt;','>').replace('\r\n',' | ')
+    q = q.replace('&quot;','"')
+    q = ' '.join(q.split())
+    n = data['id']
+    s = data['rating']
+    u = '%s/?%s' % (rashurl, n)
+    maybe = '#%s(%s): [%s] %s' % (n,s,u,q)
+    if len(maybe) < maxlen:
+        return maybe
+    return '#%s(%s): [ %s ] (long quote, see web page)' % (n,s,u)
+        
+# http://stackoverflow.com/questions/275174/how-do-i-perform-html-decoding-encoding-using-python-django
+# http://stackoverflow.com/questions/1061697/whats-the-easiest-way-to-escape-html-in-python
+def wash_quote(line, newline='|'):
+    '''
+    Return \n-separted string suitable for adding to qdb.
+
+    It tries to find breaks based on "|" characters and "<nick>" markup.
+    '''
+    out = []
+    for word in line.split():
+        word = word.strip()
+
+        if word == newline:
+            out.append('\n')
+            continue
+
+        if re.search(r'<\w+>', word):
+            out.append('\n')
+        out.append(word)
+        continue
+    return cgi.escape(' '.join(out))
+
+
+def upload_file(filename):
+    '''
+    Load a text file with one quote per line
+    '''
+    fp = open(filename)
+    for line in fp.readlines():
+        quote = wash_quote(line.strip()).strip()
+        data = apicall('add', quote=quote)
+        print data
+        continue
+    return
+
+if '__main__' == __name__:
+    import sys
+    ### use carefuly!
+    #upload_file(sys.argv[1])
+
+    
+    
+    
