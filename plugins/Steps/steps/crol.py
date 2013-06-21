d397b2c51dc994304ea66a52f2dcc920dfd12067
Frumious Bandersnatch <frumious.irc@gmail.com>
2013-06-21 02:05:06 +0200

    Add Zippy and some more steps.

---
+++ plugins/Steps/steps/crol.py
@@ -0,0 +1,12 @@
+name = 'crol'
+url = ''
+abbrevs = ['crol', '7rules']
+steps = [
+    'Make peace with your past so that it will not screw up your future.',
+    'What others think of you is none of your business.',
+    'Time heals almost anything.  Give it time.',
+    'Do not compare your life to others and do not judge them.  You have no idea what their journey is about.',
+    'Stop worrying too much.  It is alright not to know the answers.  They will come to you when you least expect it.',
+    'No one is in charge of your happiness, except you.',
+    'Smile, you do not own all of the problems in the world.',
+]
