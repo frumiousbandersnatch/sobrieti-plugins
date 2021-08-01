d662c85e656894a5c9f898b9133de1f4c960c891
Frumious Bandersnatch <frumious.irc@gmail.com>
2021-08-01 10:00:37 -0400

    More 2to3

--- plugins/Etymology/__init__.py
+++ plugins/Etymology/__init__.py
@@ -4,9 +4,10 @@ Consults http://www.etymonline.com/ for the meaning of words.
 
 import supybot
 import supybot.world as world
-import config
-import plugin
-reload(plugin) # In case we're being reloaded.
+from . import config
+from . import plugin
+import importlib
+importlib.reload(plugin) # In case we're being reloaded.
 
 Class = plugin.Class
 configure = config.configure
--- plugins/Etymology/plugin.py
+++ plugins/Etymology/plugin.py
@@ -1,39 +1,45 @@
-from urllib import urlencode
-from urllib2 import urlopen, HTTPError
+from urllib.parse import urlencode
+
+from re import sub
+from requests import get
+from html import unescape
 
 from supybot.commands import *
-import supybot.plugins as plugins
-import supybot.ircmsgs as ircmsgs
-import supybot.ircutils as ircutils
+
+
+
 import supybot.callbacks as callbacks
 
-from BeautifulSoup import BeautifulSoup
 
 def lookup(word):
     from socket import setdefaulttimeout
     setdefaulttimeout(60)
-    url = "http://www.etymonline.com/index.php?%s" \
-        % urlencode({'search':word, 'searchmode':'or'})
+    url = "http://www.etymonline.com/search?%s" \
+        % urlencode({'q':word})
 
-    doc = None
-    try:
-        doc = urlopen(url)
-    except HTTPError, e:
-        return 'http error %s for %s' % (e.code, url)
-
-    soup =  BeautifulSoup(doc)
-    dd = soup.find('dd', 'highlight')
-
-    if dd: 
-        etym = u''
-        for part in dd:
-            try:
-                etym += part.string
-            except:
-                pass
-        return etym.strip()
-    else:
+    ety = get(url)
+    if ety.status_code != 200:
         return "%s not found" % word
+
+    # thank you sopel hackers!
+    # https://github.com/sopel-irc/sopel/pull/1432/files
+    # Let's find it
+    start = ety.text.find("word__defination")
+    start = ety.text.find("<p>", start)
+    stop = ety.text.find("</p>", start)
+    sentence = ety.text[start + 3:stop]
+    # Clean up
+    sentence = unescape(sentence)
+    sentence = sub('<[^<]+?>', '', sentence)
+
+    maxlength = 275
+    if len(sentence) > maxlength:
+        sentence = sentence[:maxlength]
+        words = sentence[:-5].split(' ')
+        words.pop()
+
+        sentence = ' '.join(words) + ' […]'
+    return sentence + ' - ' + url
 
 class Etymology(callbacks.Privmsg):
 
@@ -41,7 +47,7 @@ class Etymology(callbacks.Privmsg):
         """etym <word> lookup the etymology for a word/phrase
         """
         etymology = lookup(''.join(args))
-        irc.reply(etymology.encode('utf-8'))
+        irc.reply(etymology)
 
 Class = Etymology 
 
